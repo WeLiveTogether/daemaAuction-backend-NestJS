bfefe9aee7f20ff75fa40c2590b0ad0c83a448ec
asdf148 <wnduf0405_@naver.com>
2021-11-09 23:34:02 +0900

    ⚡️ :: Connect to socket.io, Module version Downgrade
    Connect to socket.io, Module version Downgrade

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { setupSwagger } from './util/swagger/setupSwagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
