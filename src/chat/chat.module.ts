2bffaed95a971fbb12e0da98f22255430fc7623f
asdf148 <wnduf0405_@naver.com>
2021-11-13 17:24:19 +0900

    :bug: :: Provide Repository
    Provide Repository

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomRepository } from './entity/room.repository';
+import { UserRepository } from 'src/auth/entity/user.repository';
+import { ProductRepository } from 'src/product/entity/product.repository';
+import { JoinRoomRepository } from './entity/joinRoom.repository';
+import { MessageRepository } from './entity/message.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      RoomRepository,
+      UserRepository,
+      ProductRepository,
+      JoinRoomRepository,
+      MessageRepository,
+    ])
+  ],
   providers: [ChatGateway, ChatService],
   controllers: [ChatController],
 })
