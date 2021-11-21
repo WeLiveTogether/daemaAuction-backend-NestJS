3fb19d4eb7dc50198d89d8c7db903fe8b8683403
asdf148 <wnduf0405_@naver.com>
2021-11-22 01:24:16 +0900

    :bug: :: CORS error
    CORS error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { setupSwagger } from './util/swagger/setupSwagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
